626b0e925bf4e664852e39bd4eedda47b154b1f3
VarzievE <edvarziev@gmail.com>
2022-11-10 17:15:04 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
         for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // Задание 2
+        for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
     }
